f18fdc9fac2cee51eda61a505538112f15906a9e
Arshad Jamal <codingspidy@gmail.com>
2022-06-12 10:43:16 +0530

    add procfile

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const axios = require("axios");
-const https = require("https");
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
 });
 
